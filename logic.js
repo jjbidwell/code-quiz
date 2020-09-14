ca8567d398888d126aa29342713fc7c299235e4f
Joshua Bidwell <jjbidwell262@gmail.com>
2020-09-14 16:42:06 -0700

    Made a timer that runs from 5 minutes and displays a message when the minutes and seconds get to zero

--- logic.js
+++ logic.js
@@ -1,1 +1,30 @@
-console.log('JavaScript Connected!')
+var minutesEl = document.querySelector("#minutes");
+var secondsEl = document.querySelector("#seconds");
+var timeUpMessage = document.querySelector("#time-up");
+var secondsLeft = 0;
+var minutesLeft = 5;
+
+function stopTimer(){
+    clearInterval(timer)
+}
+
+var timer = setInterval(function(){
+    if(secondsLeft === 0){
+        secondsLeft = 59;
+        minutesLeft--
+        minutesEl.textContent = minutesLeft;
+    } else (
+        secondsLeft--
+    )
+
+    if(secondsLeft < 10){
+        secondsEl.textContent = "0" + secondsLeft.toString();
+    } else {
+        secondsEl.textContent = secondsLeft;
+    }
+
+    if(secondsLeft === 0 && minutesLeft ===0){
+        stopTimer();
+        timeUpMessage.style.display = "initial";
+    }
+}, 1000);
