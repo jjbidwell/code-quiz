d3c70628ac4a76abe13a921d5f5db5d5bd0d0c99
Joshua Bidwell <jjbidwell262@gmail.com>
2020-09-14 17:19:13 -0700

    made the timer start with a button push. Button becomes hidden and timer becomes visible when pressed. Timer goes invisible and time up message shows when the time is up

--- logic.js
+++ logic.js
@@ -1,30 +1,38 @@
+var timer = document.querySelector('#timer');
 var minutesEl = document.querySelector("#minutes");
 var secondsEl = document.querySelector("#seconds");
 var timeUpMessage = document.querySelector("#time-up");
+var startButton = document.querySelector("#start-button");
 var secondsLeft = 0;
 var minutesLeft = 5;
 
-function stopTimer(){
-    clearInterval(timer)
+
+
+startButton.addEventListener('click', function(){
+    startButton.style.display = "none";
+    timer.style.display = "initial";
+    var quizTimer = setInterval(function(){
+        if(secondsLeft === 0 && minutesLeft !== 0){
+            secondsLeft = 59;
+            minutesLeft--
+            minutesEl.textContent = minutesLeft;
+        } else if(secondsLeft === 0 && minutesLeft === 0){
+            stopTimer();
+            timeUpMessage.style.display = "initial";
+            timer.style.display = "none";
+        } else {
+            secondsLeft--
+        }
+
+        if(secondsLeft < 10){
+            secondsEl.textContent = "0" + secondsLeft.toString();
+        } else {
+            secondsEl.textContent = secondsLeft;
+        }
+    }, 1000);
+
+    function stopTimer(){
+    clearInterval(quizTimer)
 }
 
-var timer = setInterval(function(){
-    if(secondsLeft === 0){
-        secondsLeft = 59;
-        minutesLeft--
-        minutesEl.textContent = minutesLeft;
-    } else (
-        secondsLeft--
-    )
-
-    if(secondsLeft < 10){
-        secondsEl.textContent = "0" + secondsLeft.toString();
-    } else {
-        secondsEl.textContent = secondsLeft;
-    }
-
-    if(secondsLeft === 0 && minutesLeft ===0){
-        stopTimer();
-        timeUpMessage.style.display = "initial";
-    }
-}, 1000);
+});
