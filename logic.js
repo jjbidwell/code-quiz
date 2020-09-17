586599b8258ad32dc6c70298750bce535f020db9
Joshua Bidwell <jjbidwell262@gmail.com>
2020-09-16 17:05:30 -0700

    fixed the timer issue, added to the stop timer function and next page function to make them work better. Also made it so that the score displays at the end, right answers are worth 10, and the final score adds all remaining seconds

--- logic.js
+++ logic.js
@@ -6,52 +6,16 @@ var startButton = document.querySelector("#start-button");
 var answerButton = document.querySelectorAll(".answer");
 var correctAnswer = document.querySelectorAll('.correct');
 var incorrectAnswer = document.querySelectorAll('.incorrect');
-
+var showingPage;
+var nextPage;
 var question1 = document.querySelector('#question-1');
 var secondsLeft = 30;
-var minutesLeft = 0;
+var minutesLeft = 2;
 var currentQuestion = 1;
 var score = 0;
 
 minutesEl.textContent = minutesLeft;
 secondsEl.textContent = secondsLeft;
-
-answerButton.forEach(function(item, index){
-    item.addEventListener('click', function(){
-        if (this.getAttribute('class').includes( " correct")){
-            nextQuestion(true);
-        } else {
-            nextQuestion(false);
-        }
-
-    })
-});
-
-function nextQuestion(rightOrWrong){
-    var showingPage = document.querySelector('#question-' + currentQuestion);
-    var nextPage = document.querySelector('#question-' + (currentQuestion + 1));
-    showingPage.style.display = "none";
-    nextPage.style.display = "inherit";
-    if (rightOrWrong === true){
-        score++
-    } else if(rightOrWrong === false){
-        secondsLeft -= 5;
-        if(secondsLeft < 0 && minutesLeft > 0){
-            minutesLeft--
-            secondsLeft = secondsLeft + 60; 
-            secondsEl.textContent = secondsLeft;
-            minutesEl.textContent = minutesLeft;
-            currentQuestion++
-        }else if(minutesLeft < 0){
-            stopTimer()
-            secondsEl.textContent = "00";
-            minutesEl.textContent = "0";
-            } 
-            
-        }
-
-    }
-
 
 
 
@@ -61,15 +25,13 @@ startButton.addEventListener('click', function(){
     startButton.style.display = "none";
     timer.style.display = "initial";
     var quizTimer = setInterval(function(){
-        if(secondsLeft >= 0 && minutesLeft > 0){
+        if(secondsLeft <= 0 && minutesLeft > 0){
             secondsLeft = 59;
             minutesLeft--
 
             minutesEl.textContent = minutesLeft;
         } else if(secondsLeft < 1 && minutesLeft <= 0){
             stopTimer();
-            timeUpMessage.style.display = "initial";
-            timer.style.display = "none";
         } else {
             secondsLeft--
         }
@@ -85,8 +47,64 @@ startButton.addEventListener('click', function(){
 
 
     function stopTimer(){
-    clearInterval(quizTimer);
-}
+        showingPage = document.querySelector('#question-' + currentQuestion);
+        showingPage.style.display = "none";
+        score = score + secondsLeft + (minutesLeft * 60);
+        timeUpMessage.style.display = "block";
+        timeUpMessage.textContent = "Your Score: " + score;
+        timer.style.display = "none";
+        clearInterval(quizTimer);
+        
+    }
+
+    function nextQuestion(rightOrWrong){
+        if(currentQuestion === 2 && rightOrWrong === true){
+            console.log('last question answered!');
+            score += 10;
+            stopTimer();
+            return;
+        } else if(currentQuestion === 2 && rightOrWrong === false){
+            console.log('last question answered!');
+            stopTimer();
+            return;
+        }
+    
+        showingPage = document.querySelector('#question-' + currentQuestion);
+        nextPage = document.querySelector('#question-' + (currentQuestion + 1));
+        console.log(nextPage);
+        showingPage.style.display = "none";
+        nextPage.style.display = "inherit";
+        
+        if(rightOrWrong === true){
+            score += 10;
+            currentQuestion++
+        } else if(rightOrWrong === false){
+            secondsLeft -= 5;
+            if(secondsLeft < 0 && minutesLeft > 0){
+                minutesLeft--
+                secondsLeft = secondsLeft + 60; 
+                secondsEl.textContent = secondsLeft;
+                minutesEl.textContent = minutesLeft;
+                currentQuestion++
+            }else if(secondsLeft <= 0 && minutesLeft <= 0){
+                secondsEl.textContent = "00";
+                minutesEl.textContent = "0";
+                stopTimer()
+                } 
+                
+            }
+        }
+
+        answerButton.forEach(function(item, index){
+            item.addEventListener('click', function(){
+                if (this.getAttribute('class').includes( " correct")){
+                    nextQuestion(true);
+                } else {
+                    nextQuestion(false);
+                }
+        
+            })
+        });
 
 });
 
